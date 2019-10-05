339cee0af19916136e7db0b4d941b1359f80739a
prince <prince18pat@gmail.com>
2019-10-05 15:29:43 +0900

    add uiview classes for components

---
+++ Bitcoin-price-ios/Classes/View/BodyView.swift
@@ -0,0 +1,21 @@
+//
+//  BodyView.swift
+//  Bitcoin-price-ios
+//
+//  Created by Prince Kumar on 2019/10/05.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class BodyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
